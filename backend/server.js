4ca968b854d2f7cdf2ccd92e4f604f052cca72f8
Imam <imimam8409@gmail.com>
2024-05-08 17:49:22 +0530

    adding code

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,18 @@
 const express = require("express");
+const dotenv = require("dotenv");
+const { chats } = require("./data/data");
 
 const app = express();
+dotenv.config();
 
 app.get("/", (req, res) => {
   res.send("Api is running");
 });
 
-app.listen(4000, console.log("Server is running"));
+app.get("/api/chat", (req, res) => {
+  res.send(chats);
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, console.log(`Server is running on port ${PORT}`));
