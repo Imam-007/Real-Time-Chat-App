9a3c60cc87a2fff75985ed57216b2fd5f06f7576
Imam <imimam8409@gmail.com>
2024-05-12 23:41:36 +0530

    adding some code

--- backend/server.js
+++ backend/server.js
@@ -2,18 +2,22 @@ const express = require("express");
 const dotenv = require("dotenv");
 const { chats } = require("./data/data");
 const connectDB = require("./config/db");
+const userRoutes = require("./routes/userRoutes");
+const { notFound, errorHandler } = require("./middleware/errorMiddleware");
 
-const app = express();
 dotenv.config();
+const app = express();
+app.use(express.json());
 connectDB();
 
 app.get("/", (req, res) => {
   res.send("Api is running");
 });
 
-app.get("/api/chat", (req, res) => {
-  res.send(chats);
-});
+app.use("/api/user", userRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
