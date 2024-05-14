74e1e3085da4d6f465555f952a19a613fb4f9507
imam-007 <imimam8409@gmail.com>
2024-05-14 13:37:22 +0530

    adding code

--- frontend/src/Pages/HomePage.js
+++ frontend/src/Pages/HomePage.js
@@ -1,47 +1,48 @@
-import React from "react";
 import {
-  Container,
   Box,
+  Container,
+  Tab,
   TabList,
+  TabPanel,
+  TabPanels,
   Tabs,
   Text,
-  Tab,
-  TabPanels,
-  TabPanel,
 } from "@chakra-ui/react";
+import { useEffect } from "react";
+import { useHistory } from "react-router";
 import Login from "../components/Authentication/Login";
 import Signup from "../components/Authentication/Signup";
 
-const HomePage = () => {
+function Homepage() {
+  const history = useHistory();
+
+  useEffect(() => {
+    const user = JSON.parse(localStorage.getItem("userInfo"));
+
+    if (user) history.push("/chats");
+  }, [history]);
+
   return (
     <Container maxW="xl" centerContent>
       <Box
-        display={"flex"}
-        justifyContent={"center"}
+        d="flex"
+        justifyContent="center"
         p={3}
-        bg={"white"}
+        bg="white"
         w="100%"
         m="40px 0 15px 0"
         borderRadius="lg"
         borderWidth="1px"
       >
-        <Text fontSize={"4xl"} fontFamily={"work sans"} color={"black"}>
+        <Text fontSize="4xl" fontFamily="Work sans">
           Talk-A-Tive
         </Text>
       </Box>
-
-      <Box
-        bg={"white"}
-        w={"100%"}
-        p={4}
-        color={"black"}
-        borderRadius={"lg"}
-        borderWidth={"1px"}
-      >
-        <Tabs variant="soft-rounded">
-          <TabList mb={"1em"}>
-            <Tab width={"50%"}>Login</Tab>
-            <Tab width={"50%"}>Sign Up</Tab>
+      <Box bg="white" w="100%" p={4} borderRadius="lg" borderWidth="1px">
+        <Tabs isFitted variant="soft-rounded">
+          <TabList mb="1em">
+            <Tab>Login</Tab>
+            <Tab>Sign Up</Tab>
           </TabList>
           <TabPanels>
             <TabPanel>
@@ -55,6 +56,6 @@ const HomePage = () => {
       </Box>
     </Container>
   );
-};
+}
 
-export default HomePage;
+export default Homepage;
