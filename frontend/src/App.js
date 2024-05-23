de83714e15901c9e03b2963f861e0fd49094b956
imam-007 <imimam8409@gmail.com>
2024-05-23 17:30:07 +0530

    add some file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,9 @@
 import "./App.css";
-import Homepage from "./Pages/Homepage";
+// import Homepage from "./Pages/Homepage";
+import Homepage from "./page/Homepage";
 import { Route } from "react-router-dom";
-import Chatpage from "./Pages/Chatpage";
+// import Chatpage from "./Pages/Chatpage";
+import Chatpage from "./page/Chatpage";
 
 function App() {
   return (
